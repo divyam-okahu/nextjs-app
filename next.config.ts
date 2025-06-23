a5c1b186bd54a3188f6263f1ba1ca84c992ac279
divyam-v <divyam.v@beehyv.com>
2025-06-23 18:14:39 +0530

    instrumentation fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: [
+    "monocle2ai",
+    "require-in-the-middle",
+    "import-in-the-middle",
+    "openai",
+    "llamaindex",
+    "langchain",
+    "@langchain/core",
+    "@langchain/openai",
+    "@langchain",
+  ],
 };
 
 export default nextConfig;
